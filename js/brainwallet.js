de5847e10deab0b091dc279fcbd6fc2e09d0bc5f
username1565 <39200328+username1565@users.noreply.github.com>
2018-06-12 16:17:44 +0300

    Fix readonly textarea in converter
    Fix readonly rextarea and base64 uploading for all files. Now this available only for RAW-data.

--- js/brainwallet.js
+++ js/brainwallet.js
@@ -35,23 +35,21 @@
 	//Default coin parameters.
 	//Bitcoin
 	
-
+/*
 	Default_COIN_NAME = "Bitcoin";
 	Default_TICKER = "BTC";
 	Default_PUBLIC_KEY_VERSION = 0x00;
     Default_PRIVATE_KEY_VERSION = 0x80;
     Default_ADDRESS_URL_PREFIX = 'http://blockchain.info';
 	Default_gen_compressed = false;
+*/
 
-
-/*
-Default_COIN_NAME = "GeertCoin";
+	Default_COIN_NAME = "GeertCoin";
 	Default_TICKER = "GEERT";
 	Default_PUBLIC_KEY_VERSION = 0x26;
     Default_PRIVATE_KEY_VERSION = 0xa6;
     Default_ADDRESS_URL_PREFIX = 'https://prohashing.com/explorer/Geertcoin/';
 	Default_gen_compressed = true;
-*/
 /*
 To change default coin:
 	1. Change this parameters;
@@ -932,6 +930,8 @@ Or select another encoding to input the text here.");
 		}else{
 			$('#src').prop('title', false);
 			$('#src').removeAttr('title');
+			$('#src').removeAttr('readonly');
+			$('#upload_source_from_file').attr('onchange', 'openFile(event, "src");');
 		}
 		
 		

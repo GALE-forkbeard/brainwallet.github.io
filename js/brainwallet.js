eb0cc69296008d1a324b42cd31c86c36ddd4271e
username1565 <39200328+username1565@users.noreply.github.com>
2018-07-21 22:31:29 +0300

    Add UTF-8 encoding to converter.
    Now UTF-8 encoded text can be correct encoded and decoded.
    UTF-8 text as ASCII symbols can be decoded to UTF as text.

--- js/brainwallet.js
+++ js/brainwallet.js
@@ -668,166 +668,11 @@ to your associated bitcoin address\n\
 	
 	
 /*
-Hex to ASCII text conversion table
-Hexadecimal	Binary	ASCII
-Character
-00	00000000	NUL
-01	00000001	SOH
-02	00000010	STX
-03	00000011	ETX
-04	00000100	EOT
-05	00000101	ENQ
-06	00000110	ACK
-07	00000111	BEL
-08	00001000	BS
-09	00001001	HT
-0A	00001010	LF
-0B	00001011	VT
-0C	00001100	FF
-0D	00001101	CR
-0E	00001110	SO
-0F	00001111	SI
-10	00010000	DLE
-11	00010001	DC1
-12	00010010	DC2
-13	00010011	DC3
-14	00010100	DC4
-15	00010101	NAK
-16	00010110	SYN
-17	00010111	ETB
-18	00011000	CAN
-19	00011001	EM
-1A	00011010	SUB
-1B	00011011	ESC
-1C	00011100	FS
-1D	00011101	GS
-1E	00011110	RS
-1F	00011111	US
-20	00100000	Space
-21	00100001	!
-22	00100010	"
-23	00100011	#
-24	00100100	$
-25	00100101	%
-26	00100110	&
-27	00100111	'
-28	00101000	(
-29	00101001	)
-2A	00101010	*
-2B	00101011	+
-2C	00101100	,
-2D	00101101	-
-2E	00101110	.
-2F	00101111	/
-30	00110000	0
-31	00110001	1
-32	00110010	2
-33	00110011	3
-34	00110100	4
-35	00110101	5
-36	00110110	6
-37	00110111	7
-38	00111000	8
-39	00111001	9
-3A	00111010	:
-3B	00111011	;
-3C	00111100	<
-3D	00111101	=
-3E	00111110	>
-3F	00111111	?
-40	01000000	@
-41	01000001	A
-42	01000010	B
-43	01000011	C
-44	01000100	D
-45	01000101	E
-46	01000110	F
-47	01000111	G
-48	01001000	H
-49	01001001	I
-4A	01001010	J
-4B	01001011	K
-4C	01001100	L
-4D	01001101	M
-4E	01001110	N
-4F	01001111	O
-50	01010000	P
-51	01010001	Q
-52	01010010	R
-53	01010011	S
-54	01010100	T
-55	01010101	U
-56	01010110	V
-57	01010111	W
-58	01011000	X
-59	01011001	Y
-5A	01011010	Z
-5B	01011011	[
-5C	01011100	\
-5D	01011101	]
-5E	01011110	^
-5F	01011111	_
-60	01100000	`
-61	01100001	a
-62	01100010	b
-63	01100011	c
-64	01100100	d
-65	01100101	e
-66	01100110	f
-67	01100111	g
-68	01101000	h
-69	01101001	i
-6A	01101010	j
-6B	01101011	k
-6C	01101100	l
-6D	01101101	m
-6E	01101110	n
-6F	01101111	o
-70	01110000	p
-71	01110001	q
-72	01110010	r
-73	01110011	s
-74	01110100	t
-75	01110101	u
-76	01110110	v
-77	01110111	w
-78	01111000	x
-79	01111001	y
-7A	01111010	z
-7B	01111011	{
-7C	01111100	|
-7D	01111101	}
-7E	01111110	~
-7F	01111111	DEL
+	ASCII using symbols with codes 	00-7F,
+	extended ASCII -				00-FF
 */
 
 //I don't see this symbols here, so link "download as binary." was been added.
-
-    // stringToBytes, exception-safe
-    function stringToBytes(str) {
-      try {
-        var bytes = Crypto.chardec.UTF8.stringToBytes(str);		//char decode
-      } catch (err) {
-        var bytes = [];
-        for (var i = 0; i < str.length; ++i)
-           bytes.push(str.charCodeAt(i));
-      }
-      return bytes;
-    }
-
-    // bytesToString, exception-safe
-    function bytesToString(bytes) {
-      try {
-        var str = Crypto.charenc.UTF8.bytesToString(bytes); 	//char encode
-      } catch (err) {
-        var str = '';
-        for (var i = 0; i < bytes.length; ++i)
-            str += String.fromCharCode(bytes[i]);
-      }
-	  
-      return str;
-    }
-	
 
     function isHex(str) {
         return !/[^0123456789abcdef]+/i.test(str);
@@ -863,6 +708,249 @@ Character
 
     function isEasy16(str) {
       return !/[^asdfghjkwertuion \r\n]+/i.test(str);
+    }
+
+	function isASCII(str, extended) {
+		return (extended ? /^[\x00-\xFF]*$/ : /^[\x00-\x7F]*$/).test(str);
+	}
+
+//__________________________________________________________________________________________________________________________
+//utf8.js
+//saved from here: https://github.com/mathiasbynens/utf8.js
+
+/*! https://mths.be/utf8js v3.0.0 by @mathias */
+;(function(root) {
+
+	var stringFromCharCode = String.fromCharCode;
+
+	// Taken from https://mths.be/punycode
+	function ucs2decode(string) {
+		var output = [];
+		var counter = 0;
+		var length = string.length;
+		var value;
+		var extra;
+		while (counter < length) {
+			value = string.charCodeAt(counter++);
+			if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
+				// high surrogate, and there is a next character
+				extra = string.charCodeAt(counter++);
+				if ((extra & 0xFC00) == 0xDC00) { // low surrogate
+					output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000);
+				} else {
+					// unmatched surrogate; only append this code unit, in case the next
+					// code unit is the high surrogate of a surrogate pair
+					output.push(value);
+					counter--;
+				}
+			} else {
+				output.push(value);
+			}
+		}
+		return output;
+	}
+
+	// Taken from https://mths.be/punycode
+	function ucs2encode(array) {
+		var length = array.length;
+		var index = -1;
+		var value;
+		var output = '';
+		while (++index < length) {
+			value = array[index];
+			if (value > 0xFFFF) {
+				value -= 0x10000;
+				output += stringFromCharCode(value >>> 10 & 0x3FF | 0xD800);
+				value = 0xDC00 | value & 0x3FF;
+			}
+			output += stringFromCharCode(value);
+		}
+		return output;
+	}
+
+	function checkScalarValue(codePoint) {
+		if (codePoint >= 0xD800 && codePoint <= 0xDFFF) {
+			throw Error(
+				'Lone surrogate U+' + codePoint.toString(16).toUpperCase() +
+				' is not a scalar value'
+			);
+		}
+	}
+	/*--------------------------------------------------------------------------*/
+
+	function createByte(codePoint, shift) {
+		return stringFromCharCode(((codePoint >> shift) & 0x3F) | 0x80);
+	}
+
+	function encodeCodePoint(codePoint) {
+		if ((codePoint & 0xFFFFFF80) == 0) { // 1-byte sequence
+			return stringFromCharCode(codePoint);
+		}
+		var symbol = '';
+		if ((codePoint & 0xFFFFF800) == 0) { // 2-byte sequence
+			symbol = stringFromCharCode(((codePoint >> 6) & 0x1F) | 0xC0);
+		}
+		else if ((codePoint & 0xFFFF0000) == 0) { // 3-byte sequence
+			checkScalarValue(codePoint);
+			symbol = stringFromCharCode(((codePoint >> 12) & 0x0F) | 0xE0);
+			symbol += createByte(codePoint, 6);
+		}
+		else if ((codePoint & 0xFFE00000) == 0) { // 4-byte sequence
+			symbol = stringFromCharCode(((codePoint >> 18) & 0x07) | 0xF0);
+			symbol += createByte(codePoint, 12);
+			symbol += createByte(codePoint, 6);
+		}
+		symbol += stringFromCharCode((codePoint & 0x3F) | 0x80);
+		return symbol;
+	}
+
+	function utf8encode(string) {
+		var codePoints = ucs2decode(string);
+		var length = codePoints.length;
+		var index = -1;
+		var codePoint;
+		var byteString = '';
+		while (++index < length) {
+			codePoint = codePoints[index];
+			byteString += encodeCodePoint(codePoint);
+		}
+		return byteString;
+	}
+
+	/*--------------------------------------------------------------------------*/
+
+	function readContinuationByte() {
+		if (byteIndex >= byteCount) {
+			throw Error('Invalid byte index');
+		}
+
+		var continuationByte = byteArray[byteIndex] & 0xFF;
+		byteIndex++;
+
+		if ((continuationByte & 0xC0) == 0x80) {
+			return continuationByte & 0x3F;
+		}
+
+		// If we end up here, it’s not a continuation byte
+		throw Error('Invalid continuation byte');
+	}
+
+	function decodeSymbol() {
+		var byte1;
+		var byte2;
+		var byte3;
+		var byte4;
+		var codePoint;
+
+		if (byteIndex > byteCount) {
+			throw Error('Invalid byte index');
+		}
+
+		if (byteIndex == byteCount) {
+			return false;
+		}
+
+		// Read first byte
+		byte1 = byteArray[byteIndex] & 0xFF;
+		byteIndex++;
+
+		// 1-byte sequence (no continuation bytes)
+		if ((byte1 & 0x80) == 0) {
+			return byte1;
+		}
+
+		// 2-byte sequence
+		if ((byte1 & 0xE0) == 0xC0) {
+			byte2 = readContinuationByte();
+			codePoint = ((byte1 & 0x1F) << 6) | byte2;
+			if (codePoint >= 0x80) {
+				return codePoint;
+			} else {
+				throw Error('Invalid continuation byte');
+			}
+		}
+
+		// 3-byte sequence (may include unpaired surrogates)
+		if ((byte1 & 0xF0) == 0xE0) {
+			byte2 = readContinuationByte();
+			byte3 = readContinuationByte();
+			codePoint = ((byte1 & 0x0F) << 12) | (byte2 << 6) | byte3;
+			if (codePoint >= 0x0800) {
+				checkScalarValue(codePoint);
+				return codePoint;
+			} else {
+				throw Error('Invalid continuation byte');
+			}
+		}
+
+		// 4-byte sequence
+		if ((byte1 & 0xF8) == 0xF0) {
+			byte2 = readContinuationByte();
+			byte3 = readContinuationByte();
+			byte4 = readContinuationByte();
+			codePoint = ((byte1 & 0x07) << 0x12) | (byte2 << 0x0C) |
+				(byte3 << 0x06) | byte4;
+			if (codePoint >= 0x010000 && codePoint <= 0x10FFFF) {
+				return codePoint;
+			}
+		}
+
+		throw Error('Invalid UTF-8 detected');
+	}
+
+	var byteArray;
+	var byteCount;
+	var byteIndex;
+	function utf8decode(byteString) {
+		byteArray = ucs2decode(byteString);
+		byteCount = byteArray.length;
+		byteIndex = 0;
+		var codePoints = [];
+		var tmp;
+		while ((tmp = decodeSymbol()) !== false) {
+			codePoints.push(tmp);
+		}
+		return ucs2encode(codePoints);
+	}
+
+	/*--------------------------------------------------------------------------*/
+
+	root.version = '3.0.0';
+	root.encode = utf8encode;
+	root.decode = utf8decode;
+
+}(typeof exports === 'undefined' ? this.utf8 = {} : exports));
+//__________________________________________________________________________________________________________________________
+//Usage and test:
+//var string = '守护村子';
+//document.write('<br> UTF-8 encoded string: ', string);								//守护村子
+//document.write('<br> UTF-8 to ASCII encoding: ', utf8.encode(string));				//å®æ¤æå­
+//document.write('<br> UTF-8 from ASCII: ', utf8.decode(utf8.encode(string)));			//守护村子
+
+    // stringToBytes, exception-safe
+    function stringToBytes(str) {
+	  if(!isASCII(str, true)){str = utf8.encode(str);} //if text not have extended ASCII encoding - then encode this as utf-8 to ASCII
+      try {
+        var bytes = Crypto.chardec.UTF8.stringToBytes(str);		//char decode
+      } catch (err) {
+        var bytes = [];
+        for (var i = 0; i < str.length; ++i)
+           bytes.push(str.charCodeAt(i));
+      }
+	  return bytes;		//return bytes
+    }
+
+    // bytesToString, exception-safe
+    function bytesToString(bytes) {
+      try {
+        var str = Crypto.charenc.UTF8.bytesToString(bytes); 	//char encode
+      } catch (err) {
+        var str = '';
+        for (var i = 0; i < bytes.length; ++i)
+            str += String.fromCharCode(bytes[i]);
+      }
+	  
+      return str;
     }
 
     function autodetect(str) {
@@ -1059,7 +1147,7 @@ Or select another encoding to input the text here.");
 
         update_toolbar(enc);
 
-        bytes = stringToBytes(str);
+        bytes = stringToBytes(str); //default byte-array from text string.
 
         var type = '';
         var addVersionByte = true; // for base58check
@@ -1118,8 +1206,7 @@ Or select another encoding to input the text here.");
                 text = Crypto.util.bytesToHex(bytes);
             } else if (to == 'text') {
                 text = bytesToString(bytes);
-				console.log(bytes);
-            } else if (to == 'rfc1751') {
+			} else if (to == 'rfc1751') {
                 text = key_to_english(pad_array(bytes,8));
             } else if (to == 'mnemonic') {
                 text = mn_encode(Crypto.util.bytesToHex(pad_array(bytes,4)));
@@ -1145,7 +1232,6 @@ Or select another encoding to input the text here.");
 		
 		if(already_encoded_b64===true){
 			var base64 = bstr;
-			console.log('bstr', bstr);
 		}else{
 			var base64 = Crypto.util.bytesToBase64(bytes);
 		}
